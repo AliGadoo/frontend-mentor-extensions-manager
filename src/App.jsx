9c0715dbb469dc87d4ba0a170830545a34be78bf
AliGadoo <aligado232@gmail.com>
2025-09-11 23:03:12 +0300

    add buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react"
 import Navbar from "./components/Navbar/Navbar.jsx"
 
 function App() {
+  
+  const [activeBtn , setActiveBtn] = useState('All');
 
   return (
     <>
@@ -8,9 +11,14 @@ function App() {
         <Navbar />
         <header>
           <h1>Extensions List</h1>
+          <div className="status_btn_container">
+            <button className= {activeBtn === 'All' ? 'default_btn active' : 'default_btn'} onClick={()=>{setActiveBtn('All')}}>All</button>
+            <button className= {activeBtn === 'Active' ? 'default_btn active' : 'default_btn'} onClick={()=>{setActiveBtn('Active')}}>Active</button>
+            <button className= {activeBtn === 'Inactive' ? 'default_btn active' : 'default_btn'} onClick={()=>{setActiveBtn('Inactive')}}>Inactive</button>
+          </div>
         </header>
         <main>
-          
+
         </main>
       </div>
     </>
