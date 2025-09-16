1eeeca9a6367c850585a77df54148f8e9ac6bca9
AliGadoo <aligado232@gmail.com>
2025-09-16 20:52:54 +0300

    improve btn rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { useState } from "react"
-import Navbar from "./components/Navbar/Navbar.jsx"
+import { useState } from "react";
+import Navbar from "./components/Navbar/Navbar.jsx";
 
 function App() {
-  
-  const [activeBtn , setActiveBtn] = useState('All');
+  const [activeBtn, setActiveBtn] = useState("All");
+  const statues = ["All", "Active", "Inactive"];
 
   return (
     <>
@@ -12,17 +12,24 @@ function App() {
         <header>
           <h1>Extensions List</h1>
           <div className="status_btn_container">
-            <button className= {activeBtn === 'All' ? 'default_btn active' : 'default_btn'} onClick={()=>{setActiveBtn('All')}}>All</button>
-            <button className= {activeBtn === 'Active' ? 'default_btn active' : 'default_btn'} onClick={()=>{setActiveBtn('Active')}}>Active</button>
-            <button className= {activeBtn === 'Inactive' ? 'default_btn active' : 'default_btn'} onClick={()=>{setActiveBtn('Inactive')}}>Inactive</button>
+            {statues.map((status) => (
+              <button
+                className={`default_btn ${
+                  activeBtn === status ? "active" : ""
+                }`}
+                onClick={() => {
+                  setActiveBtn(status);
+                }}
+              >
+                {status}
+              </button>
+            ))}
           </div>
         </header>
-        <main>
-
-        </main>
+        <main></main>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
